59e536d25479c20a3ad1423b31d132cd4571c089
Divya Gupta <divgupta@expediagroup.com>
2022-12-02 17:54:57 +0530

    add day 1 and day 2

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -2,13 +2,37 @@ package day01
 
 import Day
 import Lines
+import kotlin.math.max
 
 class Day1 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        var sum = 0
+        var curSum = 0
+        for(line in input) {
+            if(line == "") {
+                sum = max(sum, curSum)
+                curSum = 0
+            } else {
+                curSum += line.toInt()
+            }
+        }
+        return max(curSum, sum)
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        var sum = mutableListOf<Int>()
+        var curSum = 0
+        for(line in input) {
+            if(line == "") {
+                sum.add(curSum)
+                curSum = 0
+            } else {
+                curSum += line.toInt()
+            }
+        }
+        sum.add(curSum)
+        sum.sort()
+        sum.forEach(System.out::println)
+        return sum.takeLast(3).sum()
     }
 }
--- calendar/day02/Day2.kt
+++ calendar/day02/Day2.kt
@@ -4,11 +4,67 @@ import Day
 import Lines
 
 class Day2 : Day() {
+
+    private val opponentMap = mapOf(
+        "A" to GameOptions.ROCK,
+        "B" to GameOptions.PAPER,
+        "C" to GameOptions.SCISSORS,
+    )
+
+    private val myMap = mapOf(
+        "X" to GameOptions.ROCK,
+        "Y" to GameOptions.PAPER,
+        "Z" to GameOptions.SCISSORS,
+    )
+
+    private val scoreMap = mapOf(
+        GameOptions.ROCK to 1,
+        GameOptions.PAPER to 2,
+        GameOptions.SCISSORS to 3,
+    )
+
+    private val winMap = mapOf(
+        GameOptions.SCISSORS to GameOptions.ROCK,
+        GameOptions.PAPER to GameOptions.SCISSORS,
+        GameOptions.ROCK to GameOptions.PAPER
+    )
+
+    private val loseMap = mapOf(
+        GameOptions.ROCK to GameOptions.SCISSORS,
+        GameOptions.SCISSORS to GameOptions.PAPER,
+        GameOptions.PAPER to GameOptions.ROCK
+    )
+
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        var score = 0
+        input.stream().map { line -> line.split(" ") }.forEach { turns ->
+            score += getScore(opponentMap[turns[0]]!!, myMap[turns[1]]!!)
+        }
+        return score
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        var score = 0
+        input.stream().map { line -> line.split(" ") }.forEach { turns ->
+            score += getScore(opponentMap[turns[0]]!!, inferMyMove(turns[1], opponentMap[turns[0]]!!))
+        }
+        return score
     }
+
+    private fun getScore(elf: GameOptions, me: GameOptions) = when {
+        elf == me -> 3 + scoreMap[me]!!
+        ((me == GameOptions.ROCK && elf == GameOptions.SCISSORS) || (me == GameOptions.SCISSORS && elf == GameOptions.PAPER) || (me == GameOptions.PAPER && elf == GameOptions.ROCK)) -> 6 + scoreMap[me]!!
+        else -> scoreMap[me]!!
+    }
+
+    private fun inferMyMove(code: String, oppMove: GameOptions) = when (code) {
+        "X" -> loseMap[oppMove]!!
+        "Y" -> oppMove
+        "Z" -> winMap[oppMove]!!
+        else -> GameOptions.INVALID
+    }
+}
+
+enum class GameOptions {
+    ROCK, PAPER, SCISSORS, INVALID
 }
