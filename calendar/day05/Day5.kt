dcd221197ecb374efa030a7993a0152099409f14
Divya Gupta <divgupta@expediagroup.com>
2022-12-06 20:19:08 +0530

    add day 5 and 6

--- calendar/day05/Day5.kt
+++ calendar/day05/Day5.kt
@@ -2,13 +2,83 @@ package day05
 
 import Day
 import Lines
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class Day5 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        var stacks = mutableListOf<MutableList<Char>>()
+        var moves = mutableListOf<MutableList<Int>>()
+        val p: Pattern = Pattern.compile("-?\\d+")
+
+        for (i in 0..8) {
+            stacks.add(mutableListOf())
+        }
+        for (line in input) {
+            if (line[1] == '1')
+                break;
+            for (i in 1 until line.length step 4) {
+                if (line[i] != ' ') {
+                    stacks[i/4].add(line[i])
+                }
+            }
+        }
+
+        for (line in input) {
+            if (line.startsWith("move")) {
+                val m: Matcher = p.matcher(line)
+                var moveList = mutableListOf<Int>()
+                while (m.find()) {
+                    moveList.add(m.group().toInt())
+                }
+                moves.add(moveList)
+            }
+        }
+
+        for(move in moves) {
+            for(i in 1..move[0]) {
+                stacks[move[2]-1].add(0, stacks[move[1]-1].removeAt(0))
+            }
+        }
+
+        return stacks.stream().map { stack -> stack[0] }.toList().joinToString("")
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        var stacks = mutableListOf<MutableList<Char>>()
+        var moves = mutableListOf<MutableList<Int>>()
+        val p: Pattern = Pattern.compile("-?\\d+")
+
+        for (i in 0..8) {
+            stacks.add(mutableListOf())
+        }
+        for (line in input) {
+            if (line[1] == '1')
+                break;
+            for (i in 1 until line.length step 4) {
+                if (line[i] != ' ') {
+                    stacks[i/4].add(line[i])
+                }
+            }
+        }
+
+        for (line in input) {
+            if (line.startsWith("move")) {
+                val m: Matcher = p.matcher(line)
+                var moveList = mutableListOf<Int>()
+                while (m.find()) {
+                    moveList.add(m.group().toInt())
+                }
+                moves.add(moveList)
+            }
+        }
+
+        for(move in moves) {
+            for(i in 1..move[0]) {
+                stacks[move[2]-1].add(i-1, stacks[move[1]-1].removeAt(0))
+            }
+        }
+
+        return stacks.stream().map { stack -> stack[0] }.toList().joinToString("")
     }
 }
--- calendar/day06/Day6.kt
+++ calendar/day06/Day6.kt
@@ -5,10 +5,18 @@ import Lines
 
 class Day6 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        for(i in 0..input[0].length-4) {
+            if (input[0].substring(i, i+4).toCharArray().distinct().count() == 4)
+                return i + 4
+        }
+        return -1
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        for(i in 0..input[0].length-14) {
+            if (input[0].substring(i, i+14).toCharArray().distinct().count() == 14)
+                return i + 14
+        }
+        return -1
     }
 }
